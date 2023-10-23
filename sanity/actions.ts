61a44985d0d54b3f4f6288d237a6497cd5d5c1fe
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-10-23 13:19:47 +0200

    sanity setup

---
+++ sanity/actions.ts
@@ -0,0 +1,21 @@
+// groq is sanity query language
+import { groq } from "next-sanity";
+import { readClient } from "./lib/client";
+
+interface GetResourcesParams {
+    query: string,
+    category: string,
+    page: string
+}
+
+export const getResources = async (params: GetResourcesParams) => {
+    const { query, category, page } = params;
+
+    try {
+        const resources = await readClient.fetch(
+            groq``
+        );
+    } catch (error) {
+        
+    }
+}
